731eba8206eff745abc29a6a661cac1e73e08130
as20203 <jzaheer.bese16seecs@seecs.edu.pk>
2021-03-16 11:17:50 +0500

    Adds function to return unlinked users.

--- linked.js
+++ linked.js
@@ -1,1 +1,6 @@
+
 // find all the users that have not been linked
+const getUnlinkedUsers = (companyUsers, linkedUsers) => {
+    const unlinkedUsers = companyUsers.filter( ({ id }) => !linkedUsers.map(({ id }) => id).includes(id));
+    return unlinkedUsers;
+}
